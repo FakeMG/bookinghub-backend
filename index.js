5e43ee080dbcbf16a17351a24f675345ad33dba3
Vũ Thanh Lâm <79557871+FakeMG@users.noreply.github.com>
2022-11-05 01:05:54 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const session = require("express-session");
+require('./authentication/auth.js');
+
+const app = express();
+
+app.use(
+    session({
+        key: "user_sid",
+        secret: "somerandonstuffs",
+        resave: false,
+        saveUninitialized: false,
+        cookie: {
+            expires: 600000,
+        },
+    })
+);
+
+app.use(express.json());       // to support JSON-encoded bodies
+app.use(express.urlencoded({ extended: true })); // to support URL-encoded bodies
+
+
+var redirectToLoginPage = (req, res, next) => {
+    if (!req.user) {
+        res.redirect("/");
+    } else {
+        next();
+    }
+};
+
+app.get('/', (req, res) => {
+    res.send('HomePage <br> <a href="/login">Login</a>');
+});
+
+app.use(require('./authentication/authentication'));
+
+app.get('/dashboard', redirectToLoginPage, (req, res) => {
+    res.json(req.session.passport);
+});
+
+app.listen(8080, () => {
+    console.log(`App is listening on port 8080`);
+});
